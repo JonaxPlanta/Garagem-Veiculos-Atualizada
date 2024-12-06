0c1abc541e45c227a96ad37b28c88ef3b728f7d0
JonaxPlanta <joao.flores3@estudante.ifms.edu.br>
2024-12-06 12:38:55 -0300

    Commiting changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // importação da biblioteca Express
 // para instalar rode o comando : npm install express
 const express = require ('express');
-
 const bodyParser = require ('body-parser');
+const fs = require('fs');
 
 // Criação de um app Express
 const app = express();
@@ -11,18 +11,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //importação das rotas
-const veiculoRotas = require('./src/routes/Veiculo');
+const rotas = require('./src/routes/Veiculo');
 
-// Config do server
+// método GET na rota raís
+app.get("/", (req, res) => {
+    // arquivo html obtido pelo fs
+    const arquivo = fs.readFileSync('./src/pages/index.html')
+
+    // resposta do servidor com arquivo html
+    res.status(200).end(arquivo);
+});
+
+// configuração do server
 const port = 8080;
 const hostname = '127.0.0.1';
 
-app.get('/', (req, res) => {
-    res.send('Está é a raiz do Servidor.')
-})
+// utilizar as rotas
+app.use('/veiculo', rotas);
 
-// Utilizar as rotas
-app.use('/veiculo', veiculoRotas);
-
-//Rodar a aplicação
-app.listen(port, hostname, console.log('Servidor rodando...'));
+// rodar a aplicação
+app.listen(port, hostname, console.log(`O servidor está rodando! \nAcesse: http://${hostname}:${port}/`));
